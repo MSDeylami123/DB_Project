efb7e24a05fa84d4a6f3119e4e2d7425d6b6034c
40217463 <soroushdeylami@gmail.com>
2025-03-16 01:18:58 +0330

    slight changes to ERD and add more tables

--- DB_Project.sql
+++ DB_Project.sql
@@ -22,6 +22,62 @@ CREATE TABLE WebsiteSupport (
     UserID INT PRIMARY KEY,
     FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
+
+CREATE TABLE Reservation (
+    ReservationID INT PRIMARY KEY,
+    UserID INT NOT NULL,
+    TicketID INT NOT NULL,
+    ReservationStatus ENUM('Reserved', 'Paid', 'Canceled') NOT NULL,
+    ReservationTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ExpirationTime TIMESTAMP NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (TicketID) REFERENCES Ticket(TicketID),
+    CHECK (ExpirationTime > ReservationTime)
+);
+
+CREATE TABLE Ticket (
+    TicketID INT PRIMARY KEY,
+    VehicleType ENUM('Plane', 'Train', 'Bus') NOT NULL,
+    Origin VARCHAR(100) NOT NULL,
+    Destination VARCHAR(100) NOT NULL,
+    DepartureTime DATETIME NOT NULL,
+    ArrivalTime DATETIME NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    Capacity INT NOT NULL,
+    CarrierID INT,
+    TravelClass ENUM('Economy', 'Business', 'VIP') NOT NULL,
+	CHECK (Price >= 0)
+);
+
+CREATE TABLE Payment (
+    PaymentID INT PRIMARY KEY,
+    UserID INT NOT NULL,
+    ReservationID INT NOT NULL,
+    Amount DECIMAL(10,2) NOT NULL CHECK (Amount >= 0),
+    PaymentMethod ENUM('Bank Card', 'Wallet', 'Cryptocurrency') NOT NULL,
+    PaymentStatus ENUM('Successful', 'Failed', 'Pending') NOT NULL,
+    PaymentTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (ReservationID) REFERENCES Reservation(ReservationID)
+);
+
+CREATE TABLE Reports (
+    ReportID INT PRIMARY KEY,
+    UserID INT NOT NULL,
+    TicketID INT,
+    ReservationID INT,
+    ReportCategory ENUM('Payment Issue', 'Travel Delay', 'Unexpected Cancellation') NOT NULL,
+    ReportText TEXT NOT NULL,
+    ProcessingStatus ENUM('Reviewed', 'Pending') DEFAULT 'Pending',
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (TicketID) REFERENCES Ticket(TicketID),
+    FOREIGN KEY (ReservationID) REFERENCES Reservation(ReservationID)
+);
+
 SELECT * FROM user;
 SELECT * FROM passenger;
 SELECT * FROM WebsiteSupport;
+SELECT * FROM Reservation;
+SELECT * FROM Ticket;
+SELECT * FROM Payment;
+SELECT * FROM Reports;
