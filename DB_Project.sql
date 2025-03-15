c364e9d2fc68de5d13fe41a5582c7efb3cd63ca7
40217463 <soroushdeylami@gmail.com>
2025-03-16 00:12:05 +0330

    fix add user table

--- DB_Project.sql
+++ DB_Project.sql
@@ -1,1 +1,15 @@
-SELECT * FROM users
+USE mydb;
+CREATE TABLE User (
+    UserID INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    Phone VARCHAR(20) UNIQUE,
+    UserType ENUM('Passenger', 'Support') NOT NULL,
+    City VARCHAR(50),
+    PasswordHash VARCHAR(255) NOT NULL,
+    RegistrationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    AccountStatus ENUM('Active', 'Inactive') DEFAULT 'Active',
+    CHECK (Email IS NOT NULL OR Phone IS NOT NULL)
+);
+SELECT * FROM user;
